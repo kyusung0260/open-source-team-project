1ec3f07dd88d2270027ab2c087744a4d3dce675c
limjunwoo04 <limjunwoo04@gmail.com>
2023-11-28 18:16:39 +0900

    jacob commmmmit

---
+++ buffalo_chess_display.py
@@ -0,0 +1,15 @@
+class Display:
+    def __init__(self):
+        self.disp=pygame.display.set_mode((game_board_width,game_board_height),pygame.DOUBLEBUF)
+        self.buffalo_img=pygame.image.load(BUFFALO_IMAGE_PATH)
+        self.hunter_img=pygame.image.load(HUNTER_IMAGE_PATH)
+        self.dog_img=pygame.image.load(DOG_IMAGE_PATH)
+
+    def gettingdisp(self):
+        return self.disp
+    def gettingBUFFALO(self):
+        return self.buffalo_img
+    def gettingHUNTER(self):
+        return self.hunter_img
+    def gettingDOG(self):
+        return self.dog_img 
